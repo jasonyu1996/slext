62f72dd5a46e241a474a1569b40dca14967eae20
Jason Yu <yuzhijingcheng1996@hotmail.com>
2021-05-11 23:21:01 +0800

    File search now checks for subsequence rather than prefix

--- app/scripts/commandpalette/filebackend.ts
+++ app/scripts/commandpalette/filebackend.ts
@@ -3,6 +3,28 @@ import { Service } from "typedi";
 import { Slext } from "../slext";
 import { File } from "../file";
 
+function filter_prefix(file: File, filter_string: string): boolean {
+    return (
+        file.path.toLowerCase().startsWith(filter_string.toLowerCase()) ||
+        file.name.toLowerCase().startsWith(filter_string.toLowerCase())
+    );
+}
+
+function filter_subseq(file: File, filter_string: string): boolean {
+    if (file.path.length < filter_string.length) return false;
+    const path: string = file.path.toLowerCase();
+    const filter_string_l: string = filter_string.toLowerCase();
+    var p: number = 0;
+    var i: number = 0;
+    while (i < filter_string_l.length) {
+        while (p < path.length && path.charAt(p) != filter_string_l.charAt(i)) p += 1;
+        if (p >= path.length) return false;
+        p += 1;
+        i += 1;
+    }
+    return true;
+}
+
 @Service()
 export class FileBackend implements CommandPaletteBackend {
     constructor(private slext: Slext) {}
@@ -16,10 +38,7 @@ export class FileBackend implements CommandPaletteBackend {
         return this.slext
             .getFiles()
             .filter(function (file, _index) {
-                return (
-                    file.path.toLowerCase().startsWith(filter.toLowerCase()) ||
-                    file.name.toLowerCase().startsWith(filter.toLowerCase())
-                );
+                return filter_subseq(file, filter);
             })
             .map(function (file): CommandItem {
                 return {
